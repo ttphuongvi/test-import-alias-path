3b57a59f9f0cb461466044b3cfeda95dc9c83f32
ttphuongvi <trthphuongvi2901@gmail.com>
2022-10-12 16:25:17 +0700

    update alias

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ const path = require("path");
 
 module.exports = {
   entry: path.resolve(__dirname, "src/index.js"),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/components"),
+    },
+  },
   output: {
     path: path.resolve(__dirname, "public"),
     filename: "bundle.js",
